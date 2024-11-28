1b150d60c45c42530e35a1f50b839aaab87c78f5
renxing <373006316@qq.com>
2024-11-29 00:03:01 +0800

    添加员工接口

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
     public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        properties.setProperty("druid.mysql.usePingMethod", "false");
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
     }
